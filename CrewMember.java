aef6fe7ac4c65a31ef949029fc97576f4ba9c33d
Jill <jill@Jills-MacBook-Pro.local>
2021-03-21 22:52:03 -0400

    added crew and crew member classes. Set up crew memembers to have name, health, occupation and created relevant methods

---
+++ CrewMember.java
@@ -0,0 +1,41 @@
+public  class CrewMember {
+    private String name;
+    CrewMemberHealth health;
+    CrewMemberOccupation occupation;
+    enum CrewMemberHealth {
+        POOR,
+        FAIR,
+        GOOD
+    }
+    enum CrewMemberOccupation {
+        ENGINEER,
+        SCIENTIST,
+        COMMUNICATIONS_OFFICER,
+        PILOT,
+        CAPTAIN
+    }
+
+    public CrewMember(String name) {
+        this.name = name;
+        this.health = CrewMemberHealth.GOOD;
+    }
+
+    public String getOccupation(){
+        return occupation.toString();
+    }
+    public String getHealth() {
+        return health.toString();
+    }
+    public String getName() {
+        return name;
+    }
+    public void setHealthToPoor(){
+        health = CrewMemberHealth.POOR;
+    }
+    public void setHealthToFair(){
+        health = CrewMemberHealth.FAIR;
+    }
+    public void setHealthToGood(){
+        health = CrewMemberHealth.GOOD;
+    }
+}
